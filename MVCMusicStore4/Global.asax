aa3e7b1f94eb64efb7336add0d1f171a008babec
Jeffrey Jones <S00991728@acad.tri-c.edu>
2020-02-10 22:40:03 -0500

    Add project files.

---
+++ MVCMusicStore4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCMusicStore4.MvcApplication" Language="C#" %>
---
+++ MVCMusicStore4/Models/MusicStoreDbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStore4.Models
+{
+    public class MusicStoreDbInitializer
+        : System.Data.Entity.DropCreateDatabaseAlways<MVCMusicStoreDB>
+    {
+        protected override void Seed(MVCMusicStoreDB context)
+        {
+            context.Artists.Add(new Artist { Name = "Al di Meola" });
+            context.Genres.Add(new Genre { Name = "Jazz" });
+            context.Albums.Add(new Album {
+                Artist = new Artist { Name = "Rush" },
+                Genre = new Genre { Name = "Rock" },
+                Price = 9.99m,
+                Title = "Caravan" });
+
+            base.Seed(context);
+        }
+    }
+}
